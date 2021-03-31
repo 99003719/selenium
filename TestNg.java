093c08ec67735add3da9bf47c032c77ea55f26b2
99003719 <69103743+99003719@users.noreply.github.com>
2021-03-31 10:30:46 +0530

    Add files via upload

---
+++ TestNg.java
@@ -0,0 +1,63 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Basics{
+@BeforeClass
+public void main() {
+System.out.println("Before");
+}
+@AfterClass
+public void after()
+{
+System.out.println("After-Method");
+}
+@Test
+public void a()
+{
+System.out.println("A");
+}
+@Test
+public void b()
+{
+System.out.println("B");
+}
+@Test
+public void c()
+{
+System.out.println("C");
+}
+}
+//....Before method and After method
+/*import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Basics {
+@BeforeMethod
+public void main() {
+System.out.println("Before");
+}
+@AfterMethod
+public void after()
+{
+System.out.println("After-Method");
+}
+@Test
+public void a()
+{
+System.out.println("A");
+}
+@Test
+public void b()
+{
+System.out.println("B");
+}
+@Test
+public void c()
+{
+System.out.println("C");
+}
+}*/

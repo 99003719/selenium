65190d17536f83477703aa672e3789f75227973d
99003719 <69103743+99003719@users.noreply.github.com>
2021-03-31 10:31:21 +0530

    Update TestNg.java

--- TestNg.java
+++ TestNg.java
@@ -30,7 +30,11 @@ public void c()
 System.out.println("C");
 }
 }
-//....Before method and After method
+
+
+//................Before method and After method
+
+
 /*import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
